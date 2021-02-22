ec7ec71012dcaef5908266f337c6d53cc9623b10
Harisha Swaminathan <hswamina@gmu.edu>
2021-02-22 16:22:46 -0500

    add enzyme to json serializer

--- src/tests/components/Contact.test.js
+++ src/tests/components/Contact.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { mount } from 'enzyme';
 import axios from 'axios';
 import contact from '../fixtures/contact';
 import Contact from '../../components/Contact';
@@ -10,7 +10,7 @@ describe('Render component and fetch from api', () => {
     jest.mock('axios');
     axios.get = jest.fn();
     axios.get.mockImplementationOnce(() => Promise.resolve(contact));
-    wrapper = shallow(<Contact key={1} contact={contact.contact} />);
+    wrapper = mount(<Contact key={1} contact={contact.contact} />);
   });
 
   test('should render Contact correctly', () => {
--- src/tests/components/Contacts.test.js
+++ src/tests/components/Contacts.test.js
@@ -1,42 +1,35 @@
 import React from 'react';
-import {shallow} from 'enzyme';
+import { shallow } from 'enzyme';
 import axios from 'axios';
 import contacts from '../fixtures/contacts';
 import Contacts from '../../components/Contacts';
 
 let wrapper;
-describe('Render component and fetch from api',()=>{
-
-  beforeEach(()=>{
+describe('Render component and fetch from api', () => {
+  beforeEach(() => {
     jest.mock('axios');
     axios.get = jest.fn();
     axios.get.mockImplementationOnce(() => Promise.resolve(contacts));
-    wrapper = shallow(<Contacts/>);
-  })
-  
+    wrapper = shallow(<Contacts />);
+  });
+
   test('should render Contacts correctly', () => {
-      expect(wrapper).toMatchSnapshot();
-  });
-  
-  test('fetches contacts from the API', async ()=>{
-      expect(wrapper.state('contacts').length).toBeGreaterThan(0);
-  });
-})
-
-
-describe('Render error message',()=>{
-  beforeEach(()=>{
-    let errorMessage = 'Request failed with status code 403';
-    axios.get.mockImplementationOnce(() =>
-      Promise.reject(new Error(errorMessage))
-    );
-    wrapper = shallow(<Contacts/>);
+    expect(wrapper).toMatchSnapshot();
   });
 
-  test('should render error message when api call fails', async ()=>{
-      expect(wrapper.state('error').length).toBeGreaterThan(0);
+  test('fetches contacts from the API', async () => {
+    expect(wrapper.state('contacts').length).toBeGreaterThan(0);
   });
-  
+});
 
-})
+describe('Render error message', () => {
+  beforeEach(() => {
+    const errorMessage = 'Request failed with status code 403';
+    axios.get.mockImplementationOnce(() => Promise.reject(new Error(errorMessage)));
+    wrapper = shallow(<Contacts />);
+  });
 
+  test('should render error message when api call fails', async () => {
+    expect(wrapper.state('error').length).toBeGreaterThan(0);
+  });
+});
