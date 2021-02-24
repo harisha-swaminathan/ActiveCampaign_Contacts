85def1a0e6f50411e7243b2f3dff9407141cfd50
Harisha Swaminathan <hswamina@gmu.edu>
2021-02-24 03:10:59 -0500

    make  Contacts component a functional component and update tests

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -1,48 +1,41 @@
 import axios from 'axios';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Contact from './Contact';
 
-export default class Contacts extends React.PureComponent {
-  constructor(props) {
-    super(props);
-    this.state = {
-      // eslint-disable-next-line react/no-unused-state
-      error: '',
-      contacts: [],
-    };
-  }
+const Contacts = () => {
+  const [contacts, setcontacts] = useState([]);
+  // eslint-disable-next-line no-unused-vars
+  const [error, setError] = useState('');
 
-  componentDidMount() {
+  useEffect(() => {
     (async () => {
       try {
-        let contacts = await axios.get('https://cors-anywhere.herokuapp.com/https://sahmed93846.api-us1.com/api/3/contacts?limit=10', {
+        let getContacts = await axios.get('https://cors-anywhere.herokuapp.com/https://sahmed93846.api-us1.com/api/3/contacts?limit=10', {
           headers: {
             'Api-Token': 'bcd062dedabcd0f1ac8a568cdcf58660c44d7e79b91763cc1a5d0c03d52c522d851fceb0',
           },
         });
-        contacts = contacts.data.contacts;
-        this.setState(() => ({ contacts }));
-      } catch (error) {
-        // eslint-disable-next-line react/no-unused-state
-        this.setState(() => ({ error: error.message }));
+        getContacts = getContacts.data.contacts;
+        setcontacts(getContacts);
+      } catch (e) {
+        setError(e.message);
       }
     })();
-  }
+  }, []);
 
-  render() {
-    const { contacts } = this.state;
-    return (
-      <>
-        {contacts.length > 0 && (
-          <tbody>
-            {contacts.map((contact) => (
-              <tr key={contact.id} className="table__row">
-                <Contact key={contact.id} contact={contact} />
-              </tr>
-            ))}
-          </tbody>
-        )}
-      </>
-    );
-  }
-}
+  return (
+    <>
+      {contacts.length > 0 && (
+      <tbody>
+        {contacts.map((contact) => (
+          <tr key={contact.id} className="table__row">
+            <Contact key={contact.id} contact={contact} />
+          </tr>
+        ))}
+      </tbody>
+      )}
+    </>
+  );
+};
+
+export default Contacts;
--- src/tests/components/Contacts.test.js
+++ src/tests/components/Contacts.test.js
@@ -1,16 +1,25 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { mount } from 'enzyme';
 import axios from 'axios';
+import { act } from 'react-dom/test-utils';
 import contacts from '../fixtures/contacts';
 import Contacts from '../../components/Contacts';
 
 let wrapper;
 describe('Render component and fetch from api', () => {
-  beforeEach(() => {
+  beforeEach(async () => {
     jest.mock('axios');
     axios.get = jest.fn();
     axios.get.mockImplementationOnce(() => Promise.resolve(contacts));
-    wrapper = shallow(<Contacts />);
+    wrapper = mount(<Contacts />, { attachTo: document.createElement('table') });
+    await act(
+      () => new Promise((resolve) => {
+        setImmediate(() => {
+          wrapper.update();
+          resolve();
+        });
+      }),
+    );
   });
 
   test('should render Contacts correctly', () => {
@@ -18,18 +27,26 @@ describe('Render component and fetch from api', () => {
   });
 
   test('fetches contacts from the API', async () => {
-    expect(wrapper.state('contacts').length).toBeGreaterThan(0);
+    expect(wrapper.find('tbody').length).toBe(1);
   });
 });
 
 describe('Render error message', () => {
-  beforeEach(() => {
+  beforeEach(async () => {
     const errorMessage = 'Request failed with status code 403';
     axios.get.mockImplementationOnce(() => Promise.reject(new Error(errorMessage)));
-    wrapper = shallow(<Contacts />);
+    wrapper = mount(<Contacts />);
+    await act(
+      () => new Promise((resolve) => {
+        setImmediate(() => {
+          wrapper.update();
+          resolve();
+        });
+      }),
+    );
   });
 
-  test('should render error message when api call fails', async () => {
-    expect(wrapper.state('error').length).toBeGreaterThan(0);
+  test('table body is not rendered when the api call fails', async () => {
+    expect(wrapper.find('tbody').length).toBe(0);
   });
 });
